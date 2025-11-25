46506653718864635f30ee0283a8546715f6a029
Akshat Kumar <akshatkumar2022@vitbhopal.ac.in>
2025-11-25 23:46:28 +0530

    syntax-fix

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -14,7 +14,7 @@ const syncUserCreation = inngest.createFunction(
     await prisma.user.create({  
       data: {
         id: data.id, 
-        email: data?.email_addresses[0]?.email_addres, 
+        email: data?.email_addresses[0]?.email_address, 
         name: data?.first_name + " " + data?.last_name, 
         image: data?.image_url,
       }
